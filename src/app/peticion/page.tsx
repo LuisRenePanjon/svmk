57469c14d574a5b8bb2e43dda30062054cadba37
Reneloper <luchorene97@gmail.com>
2025-02-07 16:56:48 -0500

    add spotify iframe

--- src/app/peticion/page.tsx
+++ src/app/peticion/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/app/utils/supabase/server";
 import { localImages } from "@/app/utils/localImages";
 import Image from "next/image";
 import PdfShower from "@/app/components/pdfShower";
+import React from "react";
 
 const groupImages = (images: string[], size: number) => {
   const grouped = [];
@@ -24,6 +25,16 @@ const Peticion = async () => {
   return (
     <div className="relative w-screen h-screen">
       <PdfShower />
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/track/3iDMvP8BZxE2Olorh2pWg9?utm_source=generator"
+        width="100%"
+        height="152"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        className={"my-3"}
+      ></iframe>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {groupedImages.map((group, index) => (
           <div key={index} className="grid gap-4">
