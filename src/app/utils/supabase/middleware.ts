4c4f6178b059b955c7802ad4bfb077e0c39c339a
Reneloper <luchorene97@gmail.com>
2025-03-04 20:02:37 -0500

    fix protection of routes

--- src/app/utils/supabase/middleware.ts
+++ src/app/utils/supabase/middleware.ts
@@ -39,13 +39,12 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    request.nextUrl.pathname != "/"
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+    url.pathname = '/'
+    return NextResponse.redirect(url.toString())
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
